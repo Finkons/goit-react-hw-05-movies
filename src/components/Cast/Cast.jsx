d2846e9cf1211f6781e8a5164445017d8d7bcdca
Andrey <finkons87@gmail.com>
2022-07-30 19:34:36 +0300

    update Cast

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -3,25 +3,23 @@ import { CastBox, List, Item, Img, Name } from './Cast.styled';
 
 export const Cast = ({ cast }) => {
   return (
-    <>
-      <CastBox>
-        <List>
-          {cast.map(({ id, original_name, profile_path }) => (
-            <Item key={id}>
-              <Img
-                src={
-                  profile_path
-                    ? `https://image.tmdb.org/t/p/w300/${profile_path}`
-                    : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'
-                }
-                alt={original_name}
-              />
-              <Name>{original_name}</Name>
-            </Item>
-          ))}
-        </List>
-      </CastBox>
-    </>
+    <CastBox>
+      <List>
+        {cast.map(({ id, original_name, profile_path }) => (
+          <Item key={id}>
+            <Img
+              src={
+                profile_path
+                  ? `https://image.tmdb.org/t/p/w300/${profile_path}`
+                  : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'
+              }
+              alt={original_name}
+            />
+            <Name>{original_name}</Name>
+          </Item>
+        ))}
+      </List>
+    </CastBox>
   );
 };
 
